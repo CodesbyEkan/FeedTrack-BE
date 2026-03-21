3dbf3d792e101070f75263be9f1543abdf52c38a
Ronald <okartercu@gmail.com>
2026-03-21 09:57:14 +0100

    local test

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cors({
   origin: ["https://guestpulse-hospitality-management.netlify.app",
-            "http://localhost:3000"
+            " http://127.0.0.1:8080"
   ],
   methods:["GET", "POST", "PUT", "DELETE"],
   credentials: true
