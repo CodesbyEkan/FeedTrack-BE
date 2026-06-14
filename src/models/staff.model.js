e0d681de5e9b37757abb832847743c949b621050
Ekan <ekanvicky@gmail.com>
2026-06-14 11:29:59 +0100

    Staff Models Pull

--- src/models/staff.model.js
+++ src/models/staff.model.js
@@ -1,28 +1,4 @@
 import mongoose from "mongoose";
-<<<<<<< HEAD
-import { type } from "node:os";
-
-const staffSchema = new mongoose.Schema({
-  fullname: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: [
-      "Front Desk",
-      "Waiter",
-      "House keeping",
-      "Security",
-      "Chef",
-      "Manager",
-      "Bartender",
-      "Driver",
-      "Others",
-    ],
-  },
-});
-=======
 
 const staffSchema = new mongoose.Schema(
   {
@@ -57,4 +33,3 @@ staffSchema.index({ fullname: 1, business: 1 }, { unique: true }); //recently ad
 const Staff = mongoose.model("Staff", staffSchema);
 
 export default Staff;
->>>>>>> 1b0eb7826f1a4f1a4611fe1c892ccc706fa7ca51
